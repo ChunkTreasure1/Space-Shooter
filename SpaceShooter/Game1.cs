e019e949743f644fe1298bc5cecb923351a0d0df
Ivar Jönsson <ivar.jonsson@shadowgamestudio.net>
2019-03-25 17:18:17 +0100

    First commit

---
+++ SpaceShooter/Game1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SpaceShooter.Gameplay.Player;
+
+namespace SpaceShooter
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager m_Graphics;
+        SpriteBatch m_SpriteBatch;
+        Player m_Player;
+
+        public Game1()
+        {
+            m_Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            //Create player
+            m_Player = new Player(new Rectangle(100, 100, 64, 64));
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            m_SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Add textures
+            m_Player.SetTexture(Content.Load<Texture2D>("Images/spaceship"));
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            GetInput();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            m_SpriteBatch.Begin();
+
+            //Draw the player
+            m_Player.Draw(ref m_SpriteBatch);
+
+            m_SpriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        private void GetInput()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                m_Player.SetPosition(m_Player.GetPosition() + new Vector2(0, -10));
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                m_Player.SetPosition(m_Player.GetPosition() + new Vector2(0, 10));
+            }
+        }
+    }
+}
---
+++ SpaceShooter/Gameplay/Player/Player.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceShooter.Gameplay.Player
+{
+    class Player
+    {
+        private Vector2 m_Position;
+        private Texture2D m_Texture;
+        private Vector2 m_Size;
+        
+        //Getting
+        public Vector2 GetPosition() { return m_Position; }
+        public void SetPosition(Vector2 pos) { m_Position = pos; }
+        
+        //Setting
+        public void SetTexture(Texture2D texture) { m_Texture = texture; }
+
+        //Constructor
+        public Player(Rectangle rect)
+        {
+            m_Position.X = rect.X;
+            m_Position.Y = rect.Y;
+
+            m_Size.X = rect.Width;
+            m_Size.Y = rect.Height;
+        }
+
+        public Player(Vector2 pos)
+        {
+            m_Position = pos;
+            m_Size = new Vector2(1, 1);
+        }
+
+        //Draws the player
+        public void Draw(ref SpriteBatch spriteBatch)
+        {
+            //Draws the texture to the screen
+            Rectangle rect = new Rectangle((int)m_Position.X, (int)m_Position.Y, (int)m_Size.X, (int)m_Size.Y);
+            spriteBatch.Draw(m_Texture, rect, Color.White);
+        }
+    }
+}
